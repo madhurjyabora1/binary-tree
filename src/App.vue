01dd0f1cd90b977e408b8abcbe86e9474bba0d7b
Madhurjya Bora <madhurjya@csuitecircle.com>
2023-06-13 19:30:34 +0530

    binary tree

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="binary-tree">
+    <input v-model="nums" />
+    <button @click="createTree">Submit</button>
+    <TreeNode :node="treeData" v-if="treeData !== null" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TreeNode from "./components/TreeNode.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TreeNode,
+  },
+  data() {
+    return {
+      nums: "",
+      treeData: null,
+    };
+  },
+  methods: {
+    createTree() {
+      const arr = this.nums.split(",").map((num) => {
+        if (num === "null") {
+          return null;
+        }
+        return parseInt(num);
+      });
+      this.treeData = this.buildTree(arr, 0);
+    },
+    buildTree(arr, index, parent = null) {
+      if (index >= arr.length || arr[index] === null) {
+        return null;
+      }
+
+      const node = {
+        value: arr[index],
+        parent: parent,
+        left: this.buildTree(arr, 2 * index + 1),
+        right: this.buildTree(arr, 2 * index + 2),
+      };
+
+      return node;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.binary-tree {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/TreeNode.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="tree-node">
+    <span>{{ node.value }}</span>
+    <div class="children" v-if="node && (node.left || node.right)">
+      <div class="child-node">
+        <TreeNode v-if="node.left" :node="node.left" />
+        <TreeNode v-if="node.right" :node="node.right" />
+      </div>
+    </div>
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: "TreeNode",
+  props: {
+    node: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
+  
+  <style scoped>
+.tree-node {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  margin: 0px;
+  min-width: 30px;
+}
+
+.children {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.child-node {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+</style>
+  
